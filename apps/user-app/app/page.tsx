67b75492e73ba367a72305c17c4cad563c07b43c
JasrajChouhan <jasrajc945@gmail.com>
2024-12-02 09:23:59 +0530

    feat(create-turbo): add db and recoil and setup

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <p  className="bg-red-700 text-6xl text-white" >Hello world</p>
+
+      <span>hell owlr</span>
+    </div>
+  );
+}
